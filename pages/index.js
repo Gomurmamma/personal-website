0c769fb27c29fd9c4febf15e2b2a6dc0b19e7136
Gomurmamma <dnaldyoung@gmail.com>
2022-05-02 16:56:51 -0400

    Layout components and styles

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,21 @@
 const Home = () => {
   return (
-    <>
+    <main>
       <h1>
-        I'm Antoino Young, a web developer based in Stroudsburg, Pennsylvania.
+        I'm
+        <br></br>
+        <span>Antoino Young</span>,<br></br>a web developer based in
+        Stroudsburg, Pennsylvania.
       </h1>
       <p>
-        I build websites form scratch and have a sharp eye for accessible
-        responsive designs
+        I build websites form scratch and have a sharp eye for accessible and
+        responsive designs.
       </p>
       <p>
         Check out my latest project <a href="#">here.</a>
       </p>
-    </>
+      <p>I'm currently available for hire.</p>
+    </main>
   );
 };
 
