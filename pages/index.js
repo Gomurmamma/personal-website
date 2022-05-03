40267f9fb022c01994edb6fc8cc737a64e3502cd
Gomurmamma <dnaldyoung@gmail.com>
2022-05-03 12:34:15 -0400

    Home page TextContent components and styling

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,23 @@
+//Style
+import style from "../public/sass/components/home.module.scss";
+
 const Home = () => {
   return (
-    <main>
-      <h1>
-        I'm
-        <br></br>
-        <span>Antoino Young</span>,<br></br>a web developer based in
-        Stroudsburg, Pennsylvania.
+    <main className={style.textcontent}>
+      <h1 className={style.textcontent__heading}>
+        I'm <span className={style.textcontent__namespan}> Antoino Young,</span>
+        a web developer based in Stroudsburg, Pennsylvania.
       </h1>
-      <p>
-        I build websites form scratch and have a sharp eye for accessible and
-        responsive designs.
+      <p className={style.textcontent__paragraph}>
+        I build websites from scratch and have a sharp eye for accessible,
+        responsive designs. Check out my latest project{" "}
+        <a href="#" className={style.textcontent__link}>
+          here.
+        </a>
+      </p>{" "}
+      <p className={style.textcontent__paragraph}>
+        I'm currently available for hire.
       </p>
-      <p>
-        Check out my latest project <a href="#">here.</a>
-      </p>
-      <p>I'm currently available for hire.</p>
     </main>
   );
 };
