4e930240e1be614fe64414a06ac664fe8e1ebc1c
Gomurmamma <dnaldyoung@gmail.com>
2022-05-07 14:05:50 -0400

    Initialized tech-icons styles and imported to component

--- components/tech-icons/tech-icons.js
+++ components/tech-icons/tech-icons.js
@@ -6,6 +6,18 @@ import NextIcon from "../../public/img/Nextjs-logo(1).svg";
 import SassIcon from "../../public/img/Sass_Logo_Color.svg";
 import JestIcon from "../../public/img/jest-seeklogo.com.svg";
 
-const TechIcons = () => {};
+//Style
+import style from "../../public/sass/components/techicons.module.scss";
+
+const TechIcons = () => {
+  return (
+    <div className={style.techicons}>
+      <ReactIcon className={style.techicons__icon} />
+      <NextIcon className={style.techicons__Nexticon} />
+      <SassIcon className={style.techicons__icon} />
+      <JestIcon className={style.techicons__icon} />
+    </div>
+  );
+};
 
 export default TechIcons;
