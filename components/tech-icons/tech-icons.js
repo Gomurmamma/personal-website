cfbe373d6ef4e35e58e84afd740051ff36e449b8
Gomurmamma <dnaldyoung@gmail.com>
2022-05-07 13:59:51 -0400

    Initialized tech-icons component

---
+++ components/tech-icons/tech-icons.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+//import SVG files
+import ReactIcon from "../../public/img/React-icon.svg";
+import NextIcon from "../../public/img/Nextjs-logo(1).svg";
+import SassIcon from "../../public/img/Sass_Logo_Color.svg";
+import JestIcon from "../../public/img/jest-seeklogo.com.svg";
+
+const TechIcons = () => {};
+
+export default TechIcons;
