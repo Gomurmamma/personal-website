009c8cd51392851bcaef5faffeedaf7859abec80
Gomurmamma <dnaldyoung@gmail.com>
2022-05-12 00:05:46 -0400

    ContactForm state defined. EmailJS dependency. WIP, need account Params. ContactForm--hidden z-index fixed.

--- components/contact-form/contact-form.js
+++ components/contact-form/contact-form.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+
+import emailjs from "@emailjs/browser";
 
 //SVG as Component
 import XIcon from "../../public/img/x-icon.svg";
@@ -7,6 +9,66 @@ import XIcon from "../../public/img/x-icon.svg";
 import style from "../../public/sass/components/contactform.module.scss";
 
 const ContactForm = ({ isDisplayForm, handleContactClick }) => {
+  //Initialize State
+  const [state, setState] = useState({
+    name: "",
+    email: "",
+    message: "",
+    buttonText: "Send",
+    error: "",
+    success: "",
+  });
+
+  //Destructure State values for ease of access
+  const { name, email, message, buttonText, error, success } = state;
+
+  //Handle State Changes
+  const handleChange = (name) => (e) => {
+    setState({
+      ...state,
+      [name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    //console.table({ name, email, message });
+
+    setState({ ...state, buttonText: "SENDING" });
+    try {
+      const templateParams = {
+        name,
+        email,
+        message,
+      };
+
+      //! UPDATE PARAMS!
+      const response = emailjs.send(
+        "<SERVICE_ID>",
+        "<TEMPLATE_ID>",
+        templateParams,
+        "<PUBLIC_KEY>"
+      );
+
+      //update state
+      setState({
+        ...state,
+        name: "",
+        email: "",
+        message: "",
+        buttonText: "Submitted",
+        success: response.data.message,
+      });
+    } catch (error) {
+      //update state
+      setState({
+        ...state,
+        buttonText: "Send",
+        error: error.response.data.error,
+      });
+    }
+  };
+
   const handleClick = () => {
     handleContactClick();
   };
@@ -20,12 +82,23 @@ const ContactForm = ({ isDisplayForm, handleContactClick }) => {
       <XIcon className={style.contactform__icon} onClick={handleClick} />
       <form id="contactform">
         <h2 className={style.contactform__title}>Contact Me</h2>
+
         <label className={style.contactform__label}>Email</label>
         <input
           type="email"
           name="email"
           className={style.contactform__input}
           placeholder="Enter your email"
+          onChange={handleChange("email")}
+        ></input>
+
+        <label className={style.contactform__label}>Name</label>
+        <input
+          type="text"
+          name="name"
+          className={style.contactform__input}
+          placeholder="Enter your preferred name"
+          onChange={handleChange("name")}
         ></input>
 
         <label className={style.contactform__label}>Message</label>
@@ -34,12 +107,17 @@ const ContactForm = ({ isDisplayForm, handleContactClick }) => {
           className={style.contactform__input}
           form="contactform"
           placeholder="Type your message"
+          onChange={handleChange("message")}
         ></textarea>
         <br></br>
+        <>
+          {error ? <div>An Error While Sending Your Message</div> : ""}
+          {success ? <div>Thank you! Your message has been sent.</div> : ""}
+        </>
         <input
           type="submit"
           form="contactform"
-          value="SUBMIT"
+          value="SEND"
           className={style.contactform__submit}
         ></input>
       </form>
